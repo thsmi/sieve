9fd00382f0549b141790634580cd61811262a112
schmid <schmid>
2008-02-10 19:19:52 +0000

    preparations for preselecting sieve account

--- sieve@mozdev.org/chrome/chromeFiles/content/SieveOverlay.js
+++ sieve@mozdev.org/chrome/chromeFiles/content/SieveOverlay.js
@@ -1,4 +1,4 @@
-function sivOpenFilters()
+function sivOpenFilters(server)
 {
   
   var w = null;
@@ -23,6 +23,7 @@ function sivOpenFilters()
     return;
   }
   
+  // use window... method
   Components
     .classes["@mozilla.org/embedcomp/window-watcher;1"]
     .getService(Components.interfaces.nsIWindowWatcher)
@@ -32,3 +33,25 @@ function sivOpenFilters()
 }
 
 
+function sivGetAccount()
+{
+  // this function depends on funtions of the overlayed message window...
+  if (typeof(GetFirstSelectedMsgFolder) == "undefined")
+    return null;
+  
+  // As we can access message window functions, we can try to retrieve... 
+  // ... the currently selected message account 
+  var server = null;
+  var folder = GetFirstSelectedMsgFolder();
+
+  if (folder)
+    server = folder.server;
+  else
+    server = accountManager.defaultAccount.incomingServer;
+
+  // make sure that we found an imap account as sieve works only with imap
+  if (server.type == "imap")
+    return server.rootMsgFolder.baseMessageURI.slice(15);
+    
+  return null;    
+}

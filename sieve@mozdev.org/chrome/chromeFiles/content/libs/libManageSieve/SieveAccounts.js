7caa5f05ad67a2e28af797d51018d4453583329b
schmid <schmid>
2010-01-24 17:43:44 +0000

    update locales

--- sieve@mozdev.org/chrome/chromeFiles/content/libs/libManageSieve/SieveAccounts.js
+++ sieve@mozdev.org/chrome/chromeFiles/content/libs/libManageSieve/SieveAccounts.js
@@ -608,6 +608,7 @@ SieveProxyInfo.prototype.getPort
 SieveProxyInfo.prototype.setPort
     = function(port)
 {
+  // TODO: Check If port is a valid integer
   gPref.setCharPref(this.prefURI+".port",port);
 }
 

2764fb7718fc7b60b91fe676941b78a081f829bf
schmid <schmid>
2010-05-17 00:15:26 +0000

    move code fragments

--- sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/SieveAtoms.js
+++ sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/SieveAtoms.js
@@ -1,3 +1,188 @@
+  // Comparators define the charset. All Sieve implementation have to support
+  // "i;octet which" is case sensitive and "i;ascii-codemap" which is case
+  // insensitive.
+
+function isSieveComparator(data,index)
+{
+  if (index == null)
+    index = 0;
+    
+  var token = data.substr(index,11).toLowerCase();
+  if (token.indexOf(":comparator") == 0)
+    return true;
+  
+  return false;
+}
+
+function SieveComparator(id)
+{
+  this.id = id;
+  this.whiteSpace = SieveLexer.createByName("whitespace");
+  this.comparator = new SieveQuotedString();
+}
+
+SieveComparator.prototype.init
+    = function (data)
+{
+  // Syntax :
+  // <":comparator"> <comparator-name: string>
+  
+  data = data.slice(":comparator".length);
+  
+  data = this.whiteSpace.init(data);
+  
+  data = this.comparator.init(data);
+  
+  return data;
+}
+
+SieveComparator.prototype.getID
+    = function ()
+{
+  return this.id;
+}    
+
+SieveComparator.prototype.toString
+    = function ()
+{
+  return ":comparator"
+    +this.whiteSpace.toString()
+    +this.comparator.toString();
+}
+
+SieveComparator.prototype.toXUL
+    = function ()
+{
+  return "Comparator - to be implemented";
+}
+/******************************************************************************/
+
+function isSieveMatchType(data,index)
+{
+  if (index == null)
+    index = 0;
+    
+  var token = data.substr(index,9).toLowerCase();
+  if (token.indexOf(":is") == 0)
+    return true;
+  if (token.indexOf(":matches") == 0)
+    return true;
+  if (token.indexOf(":contains") == 0)
+    return true;
+  
+  return false;
+}
+
+function SieveMatchType(id)
+{
+  this.id = id;
+  this.type = null;
+}
+
+SieveMatchType.prototype.init
+    = function (data)
+{
+  var token = data.substr(0,9).toLowerCase();
+  if (token.indexOf(":is") == 0)
+    this.type = "is";
+  else if (token.indexOf(":matches") == 0)
+    this.type = "matches";
+  else if (token.indexOf(":contains") == 0)
+    this.type = "contains"
+  else 
+    throw "Syntaxerror, unknown match type";
+  
+  return data.slice(this.type.length+1);
+}
+
+SieveMatchType.prototype.getID
+    = function ()
+{
+  return this.id;
+}
+
+SieveMatchType.prototype.toString
+    = function ()
+{
+  if (this.type == null)
+    return "";
+    
+  return ":"+this.type;
+}
+
+SieveMatchType.prototype.toXUL
+    = function ()
+{
+  return "<html:div class='SieveMatchType'>"
+    + "<html:option "+((this.type=="is")?"selected":"")+">is</html:option>" 
+    + "<html:option "+((this.type=="matches")?"selected":"")+">matches</html:option>" 
+    + "<html:option "+((this.type=="contains")?"selected":"")+">contains</html:option>"
+    + "</html:div>"
+}
+
+/******************************************************************************/
+
+//":localpart" / ":domain" / ":all"
+
+function isSieveAddressPart(data,index)
+{
+  if (index == null)
+    index = 0;
+    
+  var token = data.substr(index,11).toLowerCase();
+  if (token.indexOf(":localpart") == 0)
+    return true;
+  if (token.indexOf(":domain") == 0)
+    return true;
+  if (token.indexOf(":all") == 0)
+    return true;
+  
+  return false;
+}
+
+function SieveAddressPart(id)
+{
+  this.id = id;
+  this.part = null;
+}
+
+SieveAddressPart.prototype.init
+    = function (data)
+{
+  var token = data.substr(0,11).toLowerCase();
+  if (token.indexOf(":localpart") == 0)
+    this.part = "localpart";
+  else if (token.indexOf(":domain") == 0)
+    this.part = "domain";
+  else if (token.indexOf(":all") == 0)
+    this.part = "all"
+  else 
+    throw "Syntaxerror, unknown address part";
+  
+  return data.slice(this.part.length+1);
+}
+
+SieveAddressPart.prototype.getID
+    = function ()
+{
+  return this.id;
+}
+
+SieveAddressPart.prototype.toString
+    = function ()
+{
+  if (this.part == null)
+    return "";
+    
+  return ":"+this.part;
+}
+
+SieveAddressPart.prototype.toXUL
+    = function ()
+{
+  return "addresspart to be implemented"
+}
+
 
  
 

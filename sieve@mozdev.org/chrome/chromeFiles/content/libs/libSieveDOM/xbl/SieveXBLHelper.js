d90651cd7f9279302ee5855e588298b72da11c87
schmid <schmid>
2010-05-17 00:17:34 +0000

    implement xbl bindings

--- sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/xbl/SieveXBLHelper.js
+++ sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/xbl/SieveXBLHelper.js
@@ -1,9 +1,13 @@
 // TODO add listeners for callsbacks...
 // ... convert to xbl
 
-// Add button to show selection source...
+// TODO add logic to dragbox for switching between editing and not editing...
+// XLB should work like an "deck"...
+// .. deck[0] is default view
+// ... stack [1] is edit view...
 
-// add logic to dragbox for switching between editing and not editing...
+
+// TODO Add button to show selection source...
 
 function createDragBox(id,listener)
 {
@@ -33,95 +37,17 @@ function createDragBox(id,listener)
   return elm;
 }
 
-// //TODO move to ovn classfile...
+// //TODO move to own classfile...
 //  with flavour -> sieve/action, sieve/test etc...
 // TODO Implement "accepts" list
 function createDropTarget(parentId,id)
 {
-      var dropTarget = document.createElement("vbox");   
-      dropTarget.appendChild(document.createTextNode("Blubber "+id));
-      
-      dropTarget.className ="SieveDropTarget";
-      dropTarget.addEventListener("dragenter", 
-        function (event) {
-          if ( ! event.dataTransfer.mozGetDataAt('sieve/action',0))
-            return;
-            
-          var node = event.target;
-          while (node && (node.className != "SieveDropTarget"))
-            node = node.parentNode;
+  var dropTarget = document.createElement("sivDropTarget");
+  
+  if (id)
+    dropTarget.setAttribute("sivSiblingId",id);
+    
+  dropTarget.setAttribute("sivOwnerId",parentId);
           
-          node.style.backgroundColor="red";             
-        },
-        false);
-        
-      dropTarget.addEventListener("dragexit",
-        function (event) {
-          if ( ! event.dataTransfer.mozGetDataAt('sieve/action',0))
-            return;
-
-          var node = event.target;
-          while (node && (node.className != "SieveDropTarget"))
-            node = node.parentNode;
-            
-          node.style.backgroundColor= null;
-        },
-        false);
-        
-      dropTarget.addEventListener("dragover",
-        function (event) { 
-          if (! event.dataTransfer.mozGetDataAt('sieve/action',0))
-            return
-
-          event.stopPropagation(); 
-          event.preventDefault();
-        },
-        false);
-        
-      dropTarget.addEventListener("dragdrop",
-        function (event) {
-          if (!event.dataTransfer.mozGetDataAt('sieve/action',0))
-            return;          
-          
-          // use an type attribute instead of className...
-          var node = event.target;
-          while (node && (node.className != "SieveDropTarget"))
-            node = node.parentNode;
-            
-          node.style.backgroundColor = null;
-          
-          // user drops element the droptarget which contains to the draged element
-          if (node == event.dataTransfer.mozGetDataAt('sieve/action',1))
-            return;
-
-          /*event.dataTransfer.mozGetDataAt('sieve/action',1).parentNode
-              .removeChild(event.dataTransfer.mozGetDataAt('sieve/action',1));*/
-             
-          node.parentNode.insertBefore(
-            //createDropTarget(parentId,id)
-            event.dataTransfer.mozGetDataAt('sieve/action',1)
-            ,node);
-          // TODO recreate drop traeget as it caches the parent id
-          node.parentNode.insertBefore(
-            event.dataTransfer.mozGetDataAt('sieve/action',0)
-            ,node);            
-           
-           // TODO ID should be an object which contains a namespace...
-           // ... inorder to retrieve a SieveDom... 
-            
-            // TODO hookup listener ...
-           var elm = dom.removeElement(
-             event.dataTransfer.mozGetDataAt('sieve/action',2));
-           
-           if (!elm)
-             throw "No Element found";
-           // -1 means append to end...
-           dom.addElement(parentId,elm,id);
-           
-           event.stopPropagation();
-           
-        },
-        false);
-        
-      return dropTarget;
+  return dropTarget;
 }

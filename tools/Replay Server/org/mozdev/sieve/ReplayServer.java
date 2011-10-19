9cdfcee419c597549b8b4802849a452dff47c104
Thomas Schmid <schmid-thomas@gmx.net>
2011-10-19 21:28:49 +0200

    Add SASL SCRAM SHA1 testcase

--- tools/Replay Server/org/mozdev/sieve/ReplayServer.java
+++ tools/Replay Server/org/mozdev/sieve/ReplayServer.java
@@ -14,12 +14,16 @@ import java.net.ServerSocket;
 public class ReplayServer
 {
   
-  static final tests test = tests.CRAMMD5;
+  static final tests test = tests.SCRAMSHA1;
   
   static boolean cyrusBug = true;
   static boolean tls = true;
   
-  static enum tests { FRAGMENTATION, REFERRAL, CRAMMD5, LOGIN }
+  // Scram SHA1 specific
+  static boolean brokenServerSignature = false;
+  static boolean inlineServerSignature = false;
+  
+  static enum tests { FRAGMENTATION, REFERRAL, CRAMMD5, LOGIN, SCRAMSHA1 }
 
 
 
@@ -40,6 +44,9 @@ public class ReplayServer
   	  case CRAMMD5:
   	    doCramMd5Test(client);
   	    break;
+      case SCRAMSHA1:
+        doScramSha1Test(client);
+        break;  	    
   	  case LOGIN:
   	    doLoginTest(client);
   	    break;
@@ -94,7 +101,7 @@ public class ReplayServer
 	private static void assertTrue(String in, String string) throws Exception
   {
     if (!in.startsWith(string))
-      throw new Exception(string+" expected but got"+in);
+      throw new Exception(string+" expected but got "+in);
   }
 
   private static void onListScript(SieveSocket sieve) throws Exception
@@ -105,7 +112,59 @@ public class ReplayServer
         "\"SCRIPT\"\r\n"
         + "OK \"Listscript completed.\"\r\n");
 	}
-	
+
+  private static void onSaslScramSha1(SieveSocket sieve) throws Exception
+  {
+    /* 
+     * We use testsqeuence from the RFC 5802
+     *
+     * C: n,,n=user,r=fyko+d2lbbFgONRv9qkxdawL
+     * S: r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096
+     * C: c=biws,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,p=v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=
+     * S: v=rmF9pqV8S7suAoZWja4dJRkFsKQ=
+     *
+     * As SCRAM is secure and this test is dumb we need to tweak/force the client to 
+     * use a a predifined nonce
+     * 
+     * nonce = fyko+d2lbbFgONRv9qkxdawL
+     * username = user
+     * password = pencil
+     * 
+     * C: biwsbj11c2VyLHI9ZnlrbytkMmxiYkZnT05Sdjlxa3hkYXdM
+     * S: cj1meWtvK2QybGJiRmdPTlJ2OXFreGRhd0wzcmZjTkhZSlkxWlZ2V1ZzN2oscz1RU1hDUitRNnNlazhiZjkyLGk9NDA5Ng==
+     * C: Yz1iaXdzLHI9ZnlrbytkMmxiYkZnT05Sdjlxa3hkYXdMM3JmY05IWUpZMVpWdldWczdqLHA9djBYOHYzQnoyVDBDSkdiSlF5RjBYK0hJNFRzPQ==
+     * S: dj1ybUY5cHFWOFM3c3VBb1pXamE0ZEpSa0ZzS1E9
+     */
+
+    assertTrue(sieve.readLine(),
+        "AUTHENTICATE \"SCRAM-SHA-1\" \"biwsbj11c2VyLHI9ZnlrbytkMmxiYkZnT05Sdjlxa3hkYXdM\"");
+    
+    sieve.sendPacket("\"cj1meWtvK2QybGJiRmdPTlJ2OXFreGRhd0wzcmZjTkhZSlkxWlZ2V1ZzN2oscz1RU1hDUitRNnNlazhiZjkyLGk9NDA5Ng==\"\r\n");
+    
+    assertTrue(sieve.readLine(), 
+        "\"Yz1iaXdzLHI9ZnlrbytkMmxiYkZnT05Sdjlxa3hkYXdMM3JmY05IWUpZMVpWdldWczdqLHA9djBYOHYzQnoyVDBDSkdiSlF5RjBYK0hJNFRzPQ==\"");
+
+    String verifier; 
+    
+    if (brokenServerSignature)
+      verifier = "\"dj1ybUY5cHFWOFM3c3VBb1pXamE0ZEpSa0ZzS0k=\"";
+    else
+      verifier = "\"dj1ybUY5cHFWOFM3c3VBb1pXamE0ZEpSa0ZzS1E9\"";
+      
+     
+    if (inlineServerSignature)
+    {
+      sieve.sendPacket("OK (SASL "+verifier+")\r\n");
+      return;      
+    }
+    
+    sieve.sendPacket(verifier+"\r\n");
+    
+    assertTrue(sieve.readLine(),"\"\"");    
+    
+    sieve.sendPacket("OK\r\n");
+  }
+  
   private static void onSaslCramMd5(SieveSocket sieve) throws Exception
   {
     assertTrue(sieve.readLine(),"AUTHENTICATE \"CRAM-MD5\"");
@@ -178,6 +237,20 @@ public class ReplayServer
   }
   
 
+  private static void doScramSha1Test(SieveSocket sieve) throws Exception
+  {
+    onInit("SCRAM-SHA-1",sieve);
+    
+    if (tls)
+      onStartTLS("SCRAM-SHA-1",sieve);
+    
+    onSaslScramSha1(sieve);
+    
+    onListScript(sieve);
+    
+    System.out.println("SCRAM-SHA-1 Test passed...");
+  }
+  
   private static void doCramMd5Test(SieveSocket sieve) throws Exception
   { 
     onInit("CRAM-MD5",sieve);
@@ -220,6 +293,8 @@ public class ReplayServer
     assertTrue(sieve.readLine(),"STARTTLS");
     
     sieve.sendPacket("OK \"Begin TLS negotiation now\"\r\n");
+    sieve.startSSL();
+    
     Thread.sleep(2000);
     
     sieve.sendPacket("\"IMPLEMENTATION\" \"1.1\"\r\n"

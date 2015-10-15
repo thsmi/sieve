9ce9f5dac53f11c1cdf6d521b02ddfa3391b215e
Thomas Schmid <schmid-thomas@gmx.net>
2015-10-15 23:04:01 +0200

    Add missing files from last checkin

---
+++ src/sieve@mozdev.org/webapp/webapp/sieve.gui.glue.js
@@ -0,0 +1,13 @@
+"use strict"
+
+if (!net)
+  var net = {}
+  
+if (!net.tschmid)
+  net.tschmid = {};
+  
+if (!net.tschmid.sieve)
+  net.tschmid.sieve = {};
+
+if (!net.tschmid.sieve)
+  net.tschmid.sieve.gui = {};

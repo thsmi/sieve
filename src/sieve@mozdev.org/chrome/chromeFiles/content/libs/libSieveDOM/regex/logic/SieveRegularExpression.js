8dfb21c95d2fcf6da5a87799cc580e1781d1d55d
Thomas Schmid <schmid-thomas@gmx.net>
2015-11-07 00:02:22 +0100

    Update Header

--- src/sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/regex/logic/SieveRegularExpression.js
+++ src/sieve@mozdev.org/chrome/chromeFiles/content/libs/libSieveDOM/regex/logic/SieveRegularExpression.js
@@ -1,10 +1,13 @@
-/* 
- * The contents of this file is licenced. You may obtain a copy of
- * the license at http://sieve.mozdev.org or request it via email 
- * from the author. Do not remove or change this comment. 
+ /*
+ * The contents of this file are licenced. You may obtain a copy of 
+ * the license at https://github.com/thsmi/sieve/ or request it via 
+ * email from the author.
+ *
+ * Do not remove or change this comment.
  * 
  * The initial author of the code is:
  *   Thomas Schmid <schmid-thomas@gmx.net>
+ *      
  */
  
 "use strict";

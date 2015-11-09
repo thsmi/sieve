5f09cfa22f8683761457cc1f58a136e4eba42d1f
Thomas Schmid <schmid-thomas@gmx.net>
2015-11-10 00:02:22 +0100

    Add Graphical editor to startup in order to simplify debugging

--- src/sieve@mozdev.org/webapp.js
+++ src/sieve@mozdev.org/webapp.js
@@ -3,6 +3,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	
   window.open("./webapp/main4.html");
+  window.open("./chrome/chromeFiles/content/libs/libSieveDOM/SieveGui.html");
   
   /*chrome.app.window.create('main.html', {
   	'id': 'SieveAppMain',

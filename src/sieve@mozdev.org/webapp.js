f5154bdd1687d9d14357ffd07fdb51c8c9aeba88
Thomas Schmid <schmid-thomas@gmx.net>
2015-11-14 15:42:48 +0100

    cleanup

--- src/sieve@mozdev.org/webapp.js
+++ src/sieve@mozdev.org/webapp.js
@@ -3,8 +3,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	
 	window.open("./webapp/main.html");
-  window.open("./webapp/main4.html");
-  window.open("./chrome/chromeFiles/content/libs/libSieveDOM/SieveGui.html");
+  window.open("./common/libSieve/SieveGui.html");
   
   /*chrome.app.window.create('main.html', {
   	'id': 'SieveAppMain',

63f8d4df94a88f1e84f8a8a66f28c43b6440dc4d
Thomas Schmid <schmid-thomas@gmx.net>
2015-11-15 03:35:37 +0100

    Update header

--- src/sieve@mozdev.org/common/editor/sieve.broker.js
+++ src/sieve@mozdev.org/common/editor/sieve.broker.js
@@ -1,3 +1,15 @@
+/*
+ * The contents of this file are licenced. You may obtain a copy of 
+ * the license at https://github.com/thsmi/sieve/ or request it via 
+ * email from the author.
+ *
+ * Do not remove or change this comment.
+ * 
+ * The initial author of the code is:
+ *   Thomas Schmid <schmid-thomas@gmx.net>
+ *      
+ */
+ 
 "use strict";
   
 /**

9c64fa738d8cc31a912ef8acf4073046e67f2535
Thomas Schmid <schmid-thomas@gmx.net>
2018-01-08 01:26:49 +0100

    Constructor should inherrit initialisation instead of duplicating it

--- src/addon/chrome/chromeFiles/content/libs/libManageSieve/SieveMozRequestBuilder.js
+++ src/addon/chrome/chromeFiles/content/libs/libManageSieve/SieveMozRequestBuilder.js
@@ -14,63 +14,64 @@
   "use strict";
 
   /* global Components */
-  const {SieveAbstractRequestBuilder} = require("./SieveAbstractRequestBuilder.js");
+  const { SieveAbstractRequestBuilder } = require("./SieveAbstractRequestBuilder.js");
 
   /**
    * @inheritdoc
-   * @constructor
    */
-  function SieveMozRequestBuilder() {
-    this.data = "";
-  }
+  class SieveMozRequestBuilder extends SieveAbstractRequestBuilder {
 
-  SieveMozRequestBuilder.prototype = Object.create(SieveAbstractRequestBuilder.prototype);
-  SieveMozRequestBuilder.prototype.constructor = SieveMozRequestBuilder;
-
-  /**
-   * Manage Sieve uses for literals UTF-8 as encoding, network sockets are usualy
-   * binary, and javascript is something in between. This means we have to convert
-   * UTF-8 into a binary by our own...
-   *
-   * @param {String} str The binary string which should be converted
-   * @return {String} The converted string in UTF8
-   *
-   * @author Thomas Schmid <schmid-thomas@gmx.net>
-   * @author Max Dittrich
-   */
-  SieveMozRequestBuilder.prototype.jsStringToByteArray = function(str)
-  {
-    // This is very old mozilla specific code, but it is robust, mature and works as expeced.
-    // It will be dropped as soon as the new code has proven to be stable.
-    if ((typeof Components !== 'undefined')
-            && (typeof Components.classes !== 'undefined')
-            && (Components.classes["@mozilla.org/intl/scriptableunicodeconverter"])) {
-
-      // ...and convert to UTF-8
-      let converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"]
-        .createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
-
-      converter.charset = "UTF-8";
-
-      return converter.convertToByteArray(str, {});
+    /**
+     * @inheritdoc
+     **/
+    constructor() {
+      super();
     }
 
-    // with chrome we have to use the TextEncoder.
-    let data = new Uint8Array(new TextEncoder("UTF-8").encode(str));
-    return Array.prototype.slice.call(data);
-  };
+    /**
+     * Manage Sieve uses for literals UTF-8 as encoding, network sockets are usualy
+     * binary, and javascript is something in between. This means we have to convert
+     * UTF-8 into a binary by our own...
+     *
+     * @param {String} str The binary string which should be converted
+     * @return {String} The converted string in UTF8
+     *
+     * @author Thomas Schmid <schmid-thomas@gmx.net>
+     * @author Max Dittrich
+     */
+    jsStringToByteArray(str) {
+      // This is very old mozilla specific code, but it is robust, mature and works as expeced.
+      // It will be dropped as soon as the new code has proven to be stable.
+      if ((typeof Components !== 'undefined')
+        && (typeof Components.classes !== 'undefined')
+        && (Components.classes["@mozilla.org/intl/scriptableunicodeconverter"])) {
 
-  SieveMozRequestBuilder.prototype.calculateByteLength = function (data) {
-    return this.jsStringToByteArray(data).length;
-  };
+        // ...and convert to UTF-8
+        let converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"]
+          .createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
 
-  SieveMozRequestBuilder.prototype.convertToBase64 = function (decoded) {
-    return btoa(decoded);
-  };
+        converter.charset = "UTF-8";
 
-  SieveMozRequestBuilder.prototype.convertFromBase64 = function (encoded) {
-    return atob(encoded);
-  };
+        return converter.convertToByteArray(str, {});
+      }
+
+      // with chrome we have to use the TextEncoder.
+      let data = new Uint8Array(new TextEncoder("UTF-8").encode(str));
+      return Array.prototype.slice.call(data);
+    }
+
+    calculateByteLength(data) {
+      return this.jsStringToByteArray(data).length;
+    }
+
+    convertToBase64(decoded) {
+      return btoa(decoded);
+    }
+
+    convertFromBase64(encoded) {
+      return atob(encoded);
+    }
+  }
 
   exports.SieveMozRequestBuilder = SieveMozRequestBuilder;
 

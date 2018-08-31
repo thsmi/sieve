c79eb514c479b8011ccf2e9e70af7cd94d4580e2
Thomas Schmid <schmid-thomas@gmx.net>
2018-08-31 12:23:03 +0200

    Add error handler to node socket connection

--- src/app/libs/libManageSieve/SieveNodeClient.js
+++ src/app/libs/libManageSieve/SieveNodeClient.js
@@ -143,6 +143,11 @@
 
       this.socket.on('connect', () => { this.onSocketConnected(); });
       this.socket.on('data', (data) => { this.onReceive(data); });
+      this.socket.on('error', (error) => {
+        // Node guarantees that close is called after error.
+        if ((this.listener) && (this.listener.onError))
+          this.listener.onError(error);
+      });
       this.socket.on('close', () => {
         this.disconnect();
 

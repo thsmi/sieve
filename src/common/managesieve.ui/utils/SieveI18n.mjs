4cf07ff84e1f202483e9be41abfb319ce3670e88
Thomas Schmid <thsmi@users.noreply.github.com>
2021-04-24 14:18:17 +0200

    Activate Czech locale (#516)

--- src/common/managesieve.ui/utils/SieveI18n.mjs
+++ src/common/managesieve.ui/utils/SieveI18n.mjs
@@ -21,12 +21,14 @@ const LANGUAGES = new Set();
 LANGUAGES.add("en-US");
 LANGUAGES.add("de-DE");
 LANGUAGES.add("hu-HU");
+LANGUAGES.add("cz-CZ");
 
 // Maps a language to a supported language.
 const LANGUAGE_MAPPING = new Map();
 LANGUAGE_MAPPING.set("en", "en-US");
 LANGUAGE_MAPPING.set("de", "de-DE");
 LANGUAGE_MAPPING.set("hu", "hu-HU");
+LANGUAGE_MAPPING.set("cz", "cz-CZ");
 
 let instance = null;
 

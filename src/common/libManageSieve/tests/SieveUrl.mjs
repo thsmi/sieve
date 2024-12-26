677b5827930112f98ccf6d99a3bcb3724d0d47e1
Thomas Schmid <schmid-thomas@gmx.net>
2024-12-26 15:16:17 +0100

    Port should be an integer

--- src/common/libManageSieve/tests/SieveUrl.mjs
+++ src/common/libManageSieve/tests/SieveUrl.mjs
@@ -26,7 +26,7 @@ suite.add("Parsing sieve://host", function () {
   const url = new SieveUrl("sieve://host");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("4190", url.getPort());
+  suite.assertEquals(4190, url.getPort());
   suite.assertNull(url.getUser());
   suite.assertNull(url.getPassword());
 });
@@ -36,7 +36,7 @@ suite.add("Parsing sieve://user@host", function () {
   const url = new SieveUrl("sieve://user@host");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("4190", url.getPort());
+  suite.assertEquals(4190, url.getPort());
   suite.assertEquals("user", url.getUser());
   suite.assertNull(url.getPassword());
 });
@@ -46,7 +46,7 @@ suite.add("Parsing sieve://user:password@host", function () {
   const url = new SieveUrl("sieve://user:password@host");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("4190", url.getPort());
+  suite.assertEquals(4190, url.getPort());
   suite.assertEquals("user", url.getUser());
   suite.assertEquals("password", url.getPassword());
 });
@@ -56,7 +56,7 @@ suite.add("Parsing sieve://host:1234", function () {
   const url = new SieveUrl("sieve://host:1234");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("1234", url.getPort());
+  suite.assertEquals(1234, url.getPort());
   suite.assertNull(null, url.getUser());
   suite.assertNull(url.getPassword());
 });
@@ -66,7 +66,7 @@ suite.add("Parsing sieve://user@host:1234", function () {
   const url = new SieveUrl("sieve://user@host:1234");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("1234", url.getPort());
+  suite.assertEquals(1234, url.getPort());
   suite.assertEquals("user", url.getUser());
   suite.assertNull(url.getPassword());
 });
@@ -76,7 +76,7 @@ suite.add("Parsing sieve://user:password@host:1234", function () {
   const url = new SieveUrl("sieve://user:password@host:1234");
 
   suite.assertEquals("host", url.getHost());
-  suite.assertEquals("1234", url.getPort());
+  suite.assertEquals(1234, url.getPort());
   suite.assertEquals("user", url.getUser());
   suite.assertEquals("password", url.getPassword());
 });
@@ -86,7 +86,21 @@ suite.add("Parsing sieve://c3.mail.example.com", function () {
   const url = new SieveUrl("sieve://c3.mail.example.com");
 
   suite.assertEquals("c3.mail.example.com", url.getHost());
-  suite.assertEquals("4190", url.getPort());
+  suite.assertEquals(4190, url.getPort());
   suite.assertEquals(null, url.getUser());
   suite.assertEquals(null, url.getPassword());
+});
+
+suite.add("Parsing invalid port sieve://c3.mail.example.com:abc", function () {
+
+  try {
+    // eslint-disable-next-line no-unused-vars
+    const url = new SieveUrl("sieve://c3.mail.example.com:abc");
+  } catch (ex) {
+    suite.assertEquals(ex.message, "Not a valid sieve url sieve://c3.mail.example.com:abc");
+    return;
+  }
+
+  suite.fail("Function should throw but did not");
+
 });
